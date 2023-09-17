106cb3cf7244448192c89e8a65d1a68035d4824f
protarie <protarie@gmail.com>
2023-09-17 09:00:14 +0900

    초기 커밋.

---
+++ BOJ_28701/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using vll = vector<pll>;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	auto s = (n * (n + 1)) / 2;
+	cout << s << '\n'
+		<< s * s << '\n'
+		<< s * s;
+
+	return 0;
+}
